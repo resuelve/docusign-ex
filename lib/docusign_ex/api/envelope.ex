7d94e10fd9b020b65a544ba1ecaa100081fd5011
Cesar Salazar <cesar@resuelve.mx>
2018-04-23 16:45:41 -0500

    Se agrega módulo para descarga de archivos

--- lib/docusign_ex/api/envelope.ex
+++ lib/docusign_ex/api/envelope.ex
@@ -5,6 +5,7 @@ defmodule DocusignEx.Api.Envelope do
 
   require Logger
   alias DocusignEx.Api.Base
+  alias DocusignEx.Api.DownloadFile
   alias HTTPoison.Response
   alias HTTPoison.Error
   alias DocusignEx.Mapper.EnvelopeMapper
@@ -46,10 +47,13 @@ defmodule DocusignEx.Api.Envelope do
     |> Base.get([], [connect_timeout: 100000, recv_timeout: 100000, timeout: 100000])
   end
 
-  @spec get_document(String.t, String.t) :: map
-  def get_document(envelope_uid, document_id) do
+  @spec download_document(String.t, String.t) :: map
+  def download_document(envelope_uid, document_id) do
     "/envelopes/#{envelope_uid}/documents/#{document_id}"
-    |> Base.get([], [connect_timeout: 100000, recv_timeout: 100000, timeout: 100000])
+    |> DownloadFile.get(
+         [],
+         [connect_timeout: 100000, recv_timeout: 100000, timeout: 100000]
+       )
   end
 
   @spec parse_envelope({:ok, %Response{}}) :: map()
