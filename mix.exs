693547d406f73e15c2346e5a19cc6c95c9862a69
Oscar Olivera <oscarolivera@mbp-de-oscar.lan>
2018-06-11 13:07:10 -0500

    Update elixir to 1.5

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule DocusignEx.Mixfile do
     [
       app: :docusign_ex,
       version: "0.1.0",
-      elixir: "~> 1.4",
+      elixir: "~> 1.5",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
