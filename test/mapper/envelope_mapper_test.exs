a6dfb80289ee0eff8076d93a64d0ca43cc9725bb
Erick Navarro <ericknavarro@resuelve.mx>
2021-11-22 21:37:33 -0500

    Refactor envelope mapper module
    An struct is used instead of a raw map, this allow to have a well
    defined structure of a envelope and make easier to use the library,
    also reduce mapping code for better maintenance
    
    Increase coverage percentage to 60%

--- test/mapper/envelope_mapper_test.exs
+++ test/mapper/envelope_mapper_test.exs
@@ -1,153 +1,77 @@
 defmodule DocusignEx.Mapper.EnvelopeMapperTest do
   use ExUnit.Case
 
-  import DocusignEx.Mapper.EnvelopeMapper
+  alias DocusignEx.Envelope
+  alias DocusignEx.Mapper.EnvelopeMapper
 
-  setup do
-    [
-      envelope: %{
-        "documents" => [
-          %{
-            "documentBase64" => "MTIzNDU2Nzg5CmFiY2RlZmdoaWprbG1ub3BxcnN0dXZ3eHl6Cg==",
-            "documentId" => "1",
-            "fileExtension" => "txt",
-            "name" => "test64.txt"
-          }
-        ],
-        "emailSubject" => "Test",
-        "recipients" => %{
-          "signers" => [
-            %{
-              "email" => "email@email.com",
-              "name" => "Name",
-              "recipientId" => 1,
-              "routingOrder" => 1,
-              "emailNotification" => %{
-                "supportedLanguage" => "es"
-              },
-              "tabs" => %{
-                "initialHereTabs" => [
-                  %{
-                    "documentId" => "1",
-                    "recipientId" => "1",
-                    "pageNumber" => "1",
-                    "xPosition" => "20",
-                    "yPosition" => "20"
-                  }
-                ],
-                "dateSignedTabs" => [
-                  %{
-                    "documentId" => "1",
-                    "recipientId" => "1",
-                    "pageNumber" => "1",
-                    "xPosition" => "32",
-                    "yPosition" => "75"
-                  },
-                  %{
-                    "documentId" => "1",
-                    "recipientId" => "1",
-                    "pageNumber" => "1",
-                    "xPosition" => "62",
-                    "yPosition" => "10"
-                  }
-                ],
-                "fullNameTabs" => [
-                  %{
-                    "documentId" => "1",
-                    "recipientId" => "1",
-                    "pageNumber" => "1",
-                    "xPosition" => "10",
-                    "yPosition" => "100"
-                  }
-                ],
-                "signHereTabs" => [
-                  %{
-                    "documentId" => "1",
-                    "recipientId" => "1",
-                    "pageNumber" => "1",
-                    "xPosition" => "25",
-                    "yPosition" => "62"
-                  }
-                ]
-              }
-            }
+  test "build_payload/1 render a envelope with documents and signers correctly" do
+    envelope = %Envelope{
+      email_subject: "test subject",
+      webhook_url: "http://localhost",
+      brand_id: "c6086cf4-1c7c-4a43-9084-928df75de8a5",
+      documents: [
+        %{
+          filename: "contract.pdf",
+          content: "some random content"
+        }
+      ],
+      signers: [
+        %{
+          lang: "it",
+          name: "test",
+          email: "test@test.com",
+          tabs: [
+            %{key: "signHereTabs", string: "SIGN_MARK", x_offset: 10, y_offset: 15}
           ]
-        },
-        "brandId" => "someUID",
-        "status" => "sent",
-        "eventNotification" => nil
-      },
-      json: %{
-        "brand_id" => "someUID",
-        "subject" => "Test",
-        "signers" => [
-          %{
-            "name" => "Name",
-            "email" => "email@email.com",
-            "documents" => [
-              %{
-                "path" => "test/utils/test64.txt",
-                "tabs" => %{
-                  "dateSignedTabs" => [
-                    %{
-                      "xPosition" => "32",
-                      "yPosition" => "75",
-                      "pageNumber" => "1"
-                    },
-                    %{
-                      "xPosition" => "62",
-                      "yPosition" => "10",
-                      "pageNumber" => "1"
-                    }
-                  ],
-                  "fullNameTabs" => [
-                    %{
-                      "xPosition" => "10",
-                      "yPosition" => "100",
-                      "pageNumber" => "1"
-                    }
-                  ],
-                  "signHereTabs" => [
-                    %{
-                      "xPosition" => "25",
-                      "yPosition" => "62",
-                      "pageNumber" => "1"
-                    }
-                  ],
-                  "initialHereTabs" => [
-                    %{
-                      "xPosition" => "20",
-                      "yPosition" => "20",
-                      "pageNumber" => "1"
-                    }
-                  ]
-                }
-              }
-            ]
-          }
-        ]
-      }
-    ]
+        }
+      ]
+    }
+
+    assert %{} = payload = EnvelopeMapper.build_payload(envelope)
+
+    refute is_nil(payload["brandId"])
+
+    assert payload
+           |> get_in(["recipients", "signers"])
+           |> hd()
+           |> get_in(["emailNotification", "supportedLanguage"]) == "it"
+
+    assert [
+             %{
+               "anchorIgnoreIfNotPresent" => "false",
+               "anchorString" => "SIGN_MARK",
+               "anchorUnits" => "pixels",
+               "anchorXOffset" => 10,
+               "anchorYOffset" => 15
+             }
+           ] =
+             payload
+             |> get_in(["recipients", "signers"])
+             |> hd()
+             |> get_in(["tabs", "signHereTabs"])
+
+    assert get_in(payload, ["eventNotification", "url"]) == "http://localhost"
+
+    assert %{
+             "documentBase64" => _encoded,
+             "documentId" => 1,
+             "name" => "contract",
+             "fileExtension" => "pdf"
+           } = hd(payload["documents"])
   end
 
-  test "Should map the data correctly to the Docusign format", data do
-    assert map(data.json) == data.envelope
-  end
+  test "build_payload/1 render a envelope with no brand, no documents and no signers" do
+    envelope = %Envelope{
+      email_subject: "test subject",
+      webhook_url: "http://localhost",
+      documents: [],
+      signers: []
+    }
 
-  test "Should add subject to envelope map", data do
-    assert add_subject(%{}, data.json) == %{"emailSubject" => "Test"}
-  end
+    assert %{} = payload = EnvelopeMapper.build_payload(envelope)
 
-  test "Should add documents", data do
-    assert add_documents(%{}, data.json) == %{
-             "documents" => [
-               %{
-                 "documentBase64" => "MTIzNDU2Nzg5CmFiY2RlZmdoaWprbG1ub3BxcnN0dXZ3eHl6Cg==",
-                 "documentId" => "1",
-                 "fileExtension" => "txt",
-                 "name" => "test64.txt"
-               }
-             ]
-           }
+    assert is_nil(payload["brandId"])
+    assert payload["documents"] == []
+    assert payload["recipients"]["signers"] == []
   end
 end
